ab9c284b07b0ecb26925af53e2271598b7f7a11d
Gusev.Maksim <Maksim.Gusev@infotecs.ru>
2022-06-30 17:53:22 +0700

    тест get

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const token = '5586636515:AAFpLAV-BtLPkKCu3rRKNfSsdk12N8KL7dU';
 const bot = new TelegramApi(token, {polling: true});
 const axios = require('axios');
 
+// bot.sendMessage(msg.from.id, "Main menu", {reply_markup: keyboards.main_menu.reply_keyboard});
+
+
+
 bot.on('message', msg => {
     const chatId = msg.chat.id;
 
@@ -11,7 +15,7 @@ bot.on('message', msg => {
         .then(res => {
             let list = res.data,
                 key = Object.keys(list)[0];
-            bot.sendMessage(chatId, `Ссылка на сегодняшний заказ, ${res.data[key].code}!`)
+            bot.sendMessage(chatId, `Привет, ${msg.from.first_name}! <br /> Ссылка на сегодняшний заказ, ${res.data[key].code}`)
         })
         .catch(error => {
             console.error(error);
