e547c67b7d58972c114c04a5b972f9df0aa28d58
Your Name <gms441@mail.ru>
2022-07-03 17:48:59 +0700

    test cron

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramApi = require('node-telegram-bot-api');
 const token = '5586636515:AAFpLAV-BtLPkKCu3rRKNfSsdk12N8KL7dU';
 const bot = new TelegramApi(token, {polling: true});
 const axios = require('axios');
+const cron = require('node-cron');
 
 bot.on('message', msg => {
     const chatId = msg.chat.id;
@@ -16,4 +17,8 @@ bot.on('message', msg => {
         .catch(error => {
             console.error(error);
         });
+
+    cron.schedule('50 17 * * *', () => {
+        bot.sendMessage(chatId, `${msg.from.first_name}, ЗДАРОВА! \nСсылка на сегодняшний заказ: ${res.data[key].code}`);
+    });
 });
